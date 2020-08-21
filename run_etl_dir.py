fb114d3f751807366cacc7c3c0056fb39d3660f6
damklis <givemelajk@vp.pl>
2020-08-21 16:28:05 +0200

    Add argparse

--- run_etl_dir.py
+++ run_etl_dir.py
@@ -1,4 +1,4 @@
-
+import argparse
 from etl_config import Config as cfg
 from framework import (
     DataSetExtractor, DataSetTransformer,
@@ -6,12 +6,22 @@ from framework import (
 )
 
 
+def parse_args():
+    parser = argparse.ArgumentParser("Choose transformation")
+    parser.add_argument(
+        "-t", "--transformation", metavar="", help="transformation" 
+    )
+    return parser.parse_args()
+
+
 @time_func
 def run_etl_dir():
 
+    args = parse_args()
+
     extractor = DataSetExtractor(cfg.FILES)
-    transformer = DataSetTransformer("test_average_scores")
-    loader = DataSetDirectoryLoader(cfg.DATA_DIR, "test_average_scores")
+    transformer = DataSetTransformer(args.transformation)
+    loader = DataSetDirectoryLoader(cfg.DATA_DIR, args.transformation)
     
     etl = ETLJob(
         extractor=extractor,
